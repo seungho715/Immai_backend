383b2f19393e20872f6cb0d6391b6b8633caafa7
seungho715 <slee7@wpi.edu>
2024-07-12 01:18:31 -0400

    post request and cors added

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001;
 
-app.use(express.json()) 
+const corsOptions = {
+  origin: /^http:\/\/localhost:\d+$/, // Allow any port on localhost
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.post('/languages', (req, res) => {
+  console.log('Received a POST request on /languages');
+  const { learningLanguage, nativeLanguage } = req.body;
+  console.log('Learning Language: ', learningLanguage);
+  console.log('Native Language: ', nativeLanguage);
+  res.send("Languages received");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
